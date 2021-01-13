91bef4321547f31dc10c7837a0b4fa2eff4277db
joao-camargo-infra <75749277+joao-camargo-infra@users.noreply.github.com>
2021-01-13 14:49:36 -0300

    Add files via upload

--- carrinho.js
+++ carrinho.js
@@ -1,9 +1,26 @@
-const startBtn = document.getElementById("btnSpeech");
+const startBtn = document.getElementById("btn-speech");
 const carrinho = document.getElementById("carrinho");
+const favoritos = document.getElementById("favoritos");
 const result = document.createElement("div");
-const processing = document.createElement("p");
+const processing = document.getElementById("voice-textbox");
+// const processing = document.getElementById("pesquisa");
 document.body.append(result);
-document.body.append(processing);
+
+function voiceSearch() {
+  document.querySelector(".div-voice").classList.add("is-active");
+  document.body.style.cssText =
+    "background-color: #8a8a8a; transition: background-color 100ms linear";
+  document.getElementById("pesquisa").style.cssText =
+    "background-color: #8a8a8a; transition: background-color 100ms linear";
+}
+
+function stopSearch() {
+  document.querySelector(".div-voice").classList.remove("is-active");
+  document.body.style.cssText =
+    "background-color: #fff; transition: background-color 100ms linear";
+  document.getElementById("pesquisa").style.cssText =
+    "background-color: #fff; transition: background-color 100ms linear";
+}
 
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -16,35 +33,28 @@ if (typeof SpeechRecognition === undefined) {
   const recognition = new SpeechRecognition();
   recognition.continuous = true;
   recognition.interimResults = true;
-  var response = null;
   recognition.onresult = (event) => {
     const last = event.results.length - 1;
     const res = event.results[last];
     const text = res[0].transcript;
-    let rawText = text.replace(/\s/g, "");
-    rawText = rawText.toLowerCase();
-    if (rawText.includes("porfavor") && res[0].confidence > 0.9) {
-      let searchQuery = text.replace("por favor", "");
-      response = process(searchQuery);
-      const p = document.createElement("p");
-      p.innerHTML = `Você disse: ${searchQuery} </br>Assistente Pessoal disse: ${response}`;
-      processing.innerHTML = "";
-      result.appendChild(p);
+    if (res.isFinal) {
+      const response = process(text);
+      processing.value = `${text}`;
+      console.log(text);
+      // processing.innerHTML = "";
       speechSynthesis.speak(new SpeechSynthesisUtterance(response));
     } else {
-      processing.innerHTML = `Escutando: ${text}`;
+      processing.value = `${text}`;
     }
   };
   let listening = false;
   toggleBtn = () => {
     if (listening) {
       recognition.stop();
-      startBtn.style.backgroundColor =
-        "rgb(" + 195 + "," + 195 + "," + 195 + ")";
+      stopSearch();
     } else {
       recognition.start();
-      startBtn.style.backgroundColor =
-        "rgb(" + 118 + "," + 118 + "," + 118 + ")";
+      voiceSearch();
       speechSynthesis.speak(new SpeechSynthesisUtterance("O que deseja?"));
     }
     listening = !listening;
@@ -53,9 +63,10 @@ if (typeof SpeechRecognition === undefined) {
 }
 
 function process(rawText) {
-  let action = rawText.replace(/\s/g, "");
-  let text = action.replace("remover", "");
-  action = action.replace(text, "");
+  rawText = rawText.replace(/\s/g, "");
+  let filter = filterAction(rawText);
+  let action = filter[0];
+  let text = filter[1];
   let response = null;
   switch (text) {
     case "sapatoverde":
@@ -65,6 +76,7 @@ function process(rawText) {
       }
       response = addCarrinho("Sapato Verde", "pSVerde");
       break;
+
     case "sapatovermelho":
       if (action === "remover") {
         response = delCarrinho("Sapato Vermelho", "pSVermelho");
@@ -72,12 +84,38 @@ function process(rawText) {
       }
       response = addCarrinho("Sapato Vermelho", "pSVermelho");
       break;
+
     case "sapatoazul":
       if (action === "remover") {
         response = delCarrinho("Sapato Azul", "pSAzul");
         break;
       }
       response = addCarrinho("Sapato azul", "pSAzul");
+      break;
+
+    case "salvar":
+      if (!carrinho.children) {
+        response = "Carrinho vazio";
+      } else {
+        let ultPedido = 1;
+        if (favoritos.children.length !== 0) {
+          ultPedido = String(favoritos.lastChild);
+          ultPedido.replace("pedido ", "");
+          ultPedido = parseInt(ultPedido) + 1;
+        }
+        let pedFav = document.createElement("li");
+        pedFav.innerHTML = `Pedido ${ultPedido}`;
+        pedFav.setAttribute("class", `pedido${ultPedido}`);
+        favoritos.append(pedFav);
+        let pedido = document.createElement("ol");
+        pedido.setAttribute("class", `itensPedido${ultPedido}`);
+        pedFav.append(pedido);
+        for (let p of carrinho.children) {
+          let itemPed = p.cloneNode(true);
+          pedido.appendChild(itemPed);
+        }
+        response = "Pedido salvo";
+      }
       break;
 
     case "carrinho":
@@ -88,20 +126,44 @@ function process(rawText) {
       }
       response = "Algo mais?";
       break;
+
     case "pare":
       response = "Tchau";
       toggleBtn();
+      break;
+
+    case "pedido":
+      if (action === "remover") {
+        const pedido = document.getElementsByClassName(text);
+        favoritos.removeChild(pedido);
+      }
   }
+
   if (!response) {
     response = "Não encontrei o produto, pode repetir?";
   }
   return response;
 }
 
+function filterAction(action) {
+  let text = action;
+  if (action.includes("remover")) {
+    text = action.replace("remover", "");
+    action = action.replace(text, "");
+  } else if (action.includes("carregar")) {
+    text = action.replace("carregar", "");
+    action = action.replace(text, "");
+    text = action;
+  }
+  return [action, text];
+}
+
+function loadPedido(ped) {}
+
 function addCarrinho(prod, classname) {
   const produto = document.getElementsByClassName(classname)[0].cloneNode(true);
   carrinho.appendChild(produto);
-  return `${prod} adicionado ao carrinho`;
+  return `${prod} Adicionado ao carrinho`;
 }
 
 function delCarrinho(prod, classname) {
