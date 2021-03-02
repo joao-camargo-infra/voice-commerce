f9b28243794a59d8ea45a5cdddae62e2c51dbcb1
joao-camargo-infra <75749277+joao-camargo-infra@users.noreply.github.com>
2021-03-02 15:07:11 -0300

    Add files via upload

--- animationControlPanel.js
+++ animationControlPanel.js
@@ -4,26 +4,26 @@ animationContainer.style.visibility = "hidden";
 const diferenca = 5;
 const speed = 1;
 const base = 30;
-const variacao = 30;
+const variacao = 10;
 
-let a = document.getElementById('a');
-let b = document.getElementById('b');
-let c = document.getElementById('c');
+let a = document.getElementById("a");
+let b = document.getElementById("b");
+let c = document.getElementById("c");
 
 let contA = 0;
 let contB = diferenca;
-let contC = 2*diferenca;
+let contC = 2 * diferenca;
 
 function step() {
-    a.style.top = (variacao*Math.sin((contA*Math.PI)/base)) + 'px';
-    b.style.top = (variacao*Math.sin((contB*Math.PI)/base)) + 'px';
-    c.style.top = (variacao*Math.sin((contC*Math.PI)/base)) + 'px';
+  a.style.top = variacao * Math.sin((contA * Math.PI) / base) + "px";
+  b.style.top = variacao * Math.sin((contB * Math.PI) / base) + "px";
+  c.style.top = variacao * Math.sin((contC * Math.PI) / base) + "px";
 
-    contA += speed;
-    contB += speed;
-    contC += speed;
+  contA += speed;
+  contB += speed;
+  contC += speed;
 
-    window.requestAnimationFrame(step);
+  window.requestAnimationFrame(step);
 }
 
 window.requestAnimationFrame(step);
