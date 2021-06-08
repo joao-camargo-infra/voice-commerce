0897db246779d181e7590ca9d0c4a03475495739
guilherme.goulart <guilherme.goulart@infracommerce.com.br>
2021-06-08 10:51:04 -0300

    Busca de voz funcional

--- carrinho.js
+++ carrinho.js
@@ -27,11 +27,9 @@ if (typeof SpeechRecognition === undefined) {
     const text = res[0].transcript;
     if (res.isFinal) {
       toggleBtn();
-      //const response = process2(text);
+      console.log(text);
       const response = process(text);
       transcriptField.value = `${text}`;
-      busca(text);
-      console.log(text);
       speechSynthesis.speak(new SpeechSynthesisUtterance(response));
     } else {
       transcriptField.value = `${text}`;
@@ -76,11 +74,11 @@ function wordCount(words) {
     toggleBtn();
 }
 
-function process2(rawtext){
+function process(rawtext){
+  while(produtos.firstChild) produtos.removeChild(produtos.firstChild);
   let response = null;
   let searchResults = busca(rawtext);
-  console.log(searchResults);
-  if(searchResults){
+  if(searchResults.length != 0){
     jQuery.each(searchResults, function(i,item){
       produtos.appendChild(document.createTextNode(item.NOME))
       jQuery.parseHTML
@@ -92,124 +90,20 @@ function process2(rawtext){
   return response;
 }
 
-function process(rawText) {
-  rawText = rawText.replace(/\s/g, "");
-  let filter = filterAction(rawText);
-  let action = filter[0];
-  let text = filter[1];
-  let response = null;
-  switch (text) {
-      case "sapatoverde":
-          if (action === "remover") {
-              response = delCarrinho("Sapato Verde", "pSVerde");
-              break;
-          }
-          response = addCarrinho("Sapato Verde", "pSVerde");
-          break;
-
-      case "sapatovermelho":
-          if (action === "remover") {
-              response = delCarrinho("Sapato Vermelho", "pSVermelho");
-              break;
-          }
-          response = addCarrinho("Sapato Vermelho", "pSVermelho");
-          break;
-
-      case "sapatoazul":
-          if (action === "remover") {
-              response = delCarrinho("Sapato Azul", "pSAzul");
-              break;
-          }
-          response = addCarrinho("Sapato azul", "pSAzul");
-          break;
-
-      case "salvar":
-          if (!carrinho.children) {
-              response = "Carrinho vazio";
-          } else {
-              let ultPedido = 1;
-              if (favoritos.children.length !== 0) {
-                  ultPedido = String(favoritos.lastChild);
-                  ultPedido.replace("pedido ", "");
-                  ultPedido = parseInt(ultPedido) + 1;
-              }
-              let pedFav = document.createElement("li");
-              pedFav.innerHTML = `Pedido ${ultPedido}`;
-              pedFav.setAttribute("class", `pedido${ultPedido}`);
-              favoritos.append(pedFav);
-              let pedido = document.createElement("ol");
-              pedido.setAttribute("class", `itensPedido${ultPedido}`);
-              pedFav.append(pedido);
-              for (let p of carrinho.children) {
-                  let itemPed = p.cloneNode(true);
-                  pedido.appendChild(itemPed);
-              }
-              response = "Pedido salvo";
-          }
-          break;
-
-      case "carrinho":
-          for (p in carrinho.children) {
-              speechSynthesis.speak(
-                  new SpeechSynthesisUtterance(carrinho.children[p].innerHTML)
-              );
-          }
-          response = "Algo mais?";
-          break;
-
-      case "pare":
-          response = "Tchau";
-          toggleBtn();
-          break;
-
-      case "pedido":
-          if (action === "remover") {
-              const pedido = document.getElementsByClassName(text);
-              favoritos.removeChild(pedido);
-          }
-  }
-
-  if (!response) {
-      response = "Não encontrei o produto, pode repetir?";
-  }
-  return response;
-}
-
-function filterAction(action) {
-  let text = action;
-  if (action.includes("remover")) {
-      text = action.replace("remover", "");
-      action = action.replace(text, "");
-  } else if (action.includes("carregar")) {
-      text = action.replace("carregar", "");
-      action = action.replace(text, "");
-      text = action;
-  }
-  return [action, text];
-}
-
-function loadPedido(ped) { }
-
-function addCarrinho(prod, classname) {
-  const produto = document.getElementsByClassName(classname)[0].cloneNode(true);
-  carrinho.appendChild(produto);
-  return `${prod} Adicionado ao carrinho`;
-}
-
 function busca(palavraChave){
+  var resp;
   jQuery.ajax({
+    async: false,
     type: 'POST',
     url: 'http://localhost:3000/produtos/',
     data: {
         busca : palavraChave
     },
+    dataType: 'json',
     success: function(items){
-      //console.log(items);
-      //return items;
-      jQuery.each(items, function(i,item){
-        produtos.appendChild(document.createTextNode(item.NOME))
-        jQuery.parseHTML
-      });
+      console.log(items);
+      resp = items;
     }
   });
+  return resp;
 }
